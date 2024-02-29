ffa805e5c64983a410088b6ddd8ce7b0eb86060b
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-02-28 22:57:56 -0800

    [Task 0 - 5] - All files

--- 2-binary_insert_right.c
+++ 2-binary_insert_right.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
+ * binary_tree_insert_right - inserts a node as the right-child of another node
  *
- * @parent: is a pointer to the node to insert the left-child in
+ * @parent: is a pointer to the node to insert the right-child in
  * @value:  is the value to store in the new node
  *
  * Return: a pointer to the created node
  * or NULL on failure or if parent is NULL
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
@@ -24,12 +24,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->left = NULL;
 	new_node->right = NULL;
 
-	if (parent->left)
+	if (parent->right)
 	{
-		new_node->left = parent->left;
-		new_node->left->parent = new_node;
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
 	}
-	parent->left = new_node;
+	parent->right = new_node;
 	new_node->parent = parent;
 
 	return (new_node);
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,27 +1,35 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of a node
+ * 
+ * @parent: pointer to the parent node of the node to be created
+ * @value: the value to be put in the new node
+ * 
+ * Return: pointer to the created node else NULL
+*/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (!new_node)
-        return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
 
-    new_node->n = value;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    if (parent->right)
-    {
-        new_node->right = parent->right;
-        parent->right = new_node;
-    }
-    else
-        parent->right = new_node;
+	if (parent->right)
+	{
+		new_node->right = parent->right;
+		new_node->right->parent = new_node;
+	}
+	parent->right = new_node;
+	new_node->parent = parent;
 
-    return (new_node);
+	return (new_node);
 }
